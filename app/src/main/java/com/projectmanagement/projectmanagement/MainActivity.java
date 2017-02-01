bb8e3996b4a619b756556df67ed5297f1af29b05
Draxxil <tobias-henriksen@hotmail.com>
2017-02-01 10:12:55 +0100

    created a button, and setup onclicklistener.

--- app/src/main/java/com/projectmanagement/projectmanagement/MainActivity.java
+++ app/src/main/java/com/projectmanagement/projectmanagement/MainActivity.java
@@ -2,6 +2,10 @@ package com.projectmanagement.projectmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText password = (EditText) findViewById(R.id.password_et);
+        password.setTransformationMethod(new PasswordTransformationMethod());
+
+        Button login = (Button) findViewById(R.id.login_btn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
